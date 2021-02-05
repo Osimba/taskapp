fa2ec7a733b660ba4e9643e48e37c6fce334ea15
Osimba <osiqua@gmail.com>
2021-02-05 10:36:24 -0700

    Test axios defaults on server

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = process.env.APP_URL;
+window.axios.defaults.baseURL = process.env.MIX_APP_URL;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
